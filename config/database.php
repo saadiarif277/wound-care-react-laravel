46f66f7744ec6be24f615444a95c6aa8d82cdf65
saadiarif277 <saadarif277@gmail.com>
2025-06-04 23:15:50 +0500

    Update database.php

--- config/database.php
+++ config/database.php
@@ -148,5 +148,18 @@ return [
         ],
 
     ],
+     'supabase' => [
+            'driver' => 'pgsql',
+            'host' => env('SUPABASE_DB_HOST'),
+            'port' => env('SUPABASE_DB_PORT', '5432'),
+            'database' => env('SUPABASE_DB_DATABASE', 'postgres'),
+            'username' => env('SUPABASE_DB_USERNAME'),
+            'password' => env('SUPABASE_DB_PASSWORD'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => env('SUPABASE_DB_SSL_MODE', 'require'),
+        ],
 
 ];
