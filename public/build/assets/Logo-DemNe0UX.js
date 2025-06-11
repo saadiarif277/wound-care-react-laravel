4480c39f7b0c8cc15bd3bc0691c281235b95f02a
saadiarif277 <saadarif277@gmail.com>
2025-06-12 00:44:51 +0500

    Changed Build
    Build Files new,

--- public/build/assets/Logo-DemNe0UX.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BIIu6Vfg.js";function n(o){return r.jsx("img",{src:"/MSC-logo.png",alt:"MSC Wound Care",...o})}export{n as L};
---
+++ public/build/assets/Logo-iuVOFOZI.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-b7vnQlZc.js";function n(o){return r.jsx("img",{src:"/MSC-logo.png",alt:"MSC Wound Care",...o})}export{n as L};
